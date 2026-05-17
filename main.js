c1c73717cceb53b6dde59cadfc08948d90b15c32
sundaysiwale <sundaysiwale4@gmail.com>
2026-05-17 19:29:28 +0200

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Created by user on 5/17/2026, 12:32:17 PM
-// Last modified by user on 5/17/2026, 1:24:25 PM
+// Last modified by user on 5/17/2026, 7:25:09 PM
 // function hello(){
 //     alert("hi");
 
@@ -8,18 +8,34 @@
 // hello();
 
 
+//switching
+// var space = 'first';
+// switch(space){
+//     case 'first':
+//         console.log('gold');
+//         break;
+//     case 'second':
+//             console.log('silver');
+//         break;
+//     case 'third':
+//         console.log('bronze');
+//         break;
+//     default:
+//         console.log('yeeeee')
+// }
 
-var space = 'first';
-switch(space){
-    case 'first':
-        console.log('gold');
-        break;
-    case 'second':
-            console.log('silver');
-        break;
-    case 'third':
-        console.log('blonze');
-        break;
-    default:
-        console.log('yeeeee')
+//rounding off math method
+
+// var decimal = Math.random() * 10;
+// console.log(decimal);
+
+// var rounded = Math.ceil(decimal);
+// console.log(rounded);
+
+var name = 'sunday';
+
+console.log(name.length);
+
+for (var i = 0;i<name.length;i++){
+    console.log(name[i]);
 }
