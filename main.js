6700588f13110d310b1ea2ed8bb7757b1cf89d2a
sundaysiwale <sundaysiwale4@gmail.com>
2026-05-17 14:24:39 +0200

    commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Created by user on 5/17/2026, 12:32:17 PM
+// Last modified by user on 5/17/2026, 1:24:25 PM
+// function hello(){
+//     alert("hi");
+
+// }
+
+// hello();
+
+
+
+var space = 'first';
+switch(space){
+    case 'first':
+        console.log('gold');
+        break;
+    case 'second':
+            console.log('silver');
+        break;
+    case 'third':
+        console.log('blonze');
+        break;
+    default:
+        console.log('yeeeee')
+}
